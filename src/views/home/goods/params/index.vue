a57e583aaf7a6e4b87ab602cd5dd65f8370fd00c
fly-sy <942093888@qq.com>
2019-07-26 11:15:09 +0800

    完善params模块的删除功能

--- src/views/home/goods/params/index.vue
+++ src/views/home/goods/params/index.vue
@@ -366,6 +366,29 @@ export default {
 
       this.$message.success('修改参数项成功！')
     },
+    // 根据Id删除对应的参数项
+    async removeParams(attrId) {
+      const confirmResult = await this.$confirm('此操作将永久删除该参数, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+
+      // 用户取消了删除的操作
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除！')
+      }
+
+      // 删除的业务逻辑
+      const { data: res } = await this.$http.delete(`categories/${this.cateId}/attributes/${attrId}`)
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除参数失败！')
+      }
+
+      this.$message.success('删除参数成功！')
+      this.getParamsData()
+    },
     showInput(row) {
       row.inputVisible = true
       console.log(this)
@@ -413,6 +436,9 @@ export default {
 
   .input-new-tag {
     width: 120px;
+  }
+  .el-table__expanded-cell span {
+    margin: 0 15px 15px 0;
   }
 }
 </style>
